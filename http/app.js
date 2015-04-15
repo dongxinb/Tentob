a5bcfc27aa37217d2dcc000a17b09a4e776cf342
pollow <pollow.xc@gmail.com>
2015-04-15 22:17:23 +0800

    API provided.

--- http/app.js
+++ http/app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var multer = require('multer');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -13,6 +14,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('env', 'development');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -21,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(multer({dest : './upload'}));
 
 app.use('/', routes);
 app.use('/users', users);
@@ -32,18 +35,22 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+app.locals.report = {};
+
 // error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    console.log("[In Dev] Dev Mode.");
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            stack: err.stack,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
