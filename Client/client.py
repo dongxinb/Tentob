a88b645cf852e87aab34ae4eb14dc05e1dc42c95
dongxinb <dongxinb@gmail.com>
2015-04-15 19:03:02 +0800

    add rsa

--- Client/client.py
+++ Client/client.py
@@ -1,18 +1,72 @@
+# -*- coding: utf-8 -*-
 #!/usr/bin/python3
 import uuid
 import urllib
 # import urllib2
 import time, threading
 import os
+import rsa
+# from M2Crypto import RSA
+
+
+def crypt(data, is_encrypt=True):
+	# publickey = rsa.importKey(open('pub.pem', 'r').read())
+	# privatekey = rsa.importKey(open('pri.pem', 'r').read())
+	# with open('pri.pem') as privatefile:
+	# 	keydata = privatefile.read()
+	# print(keydata)
+	# keydata = keydata.encode(encoding="utf-8")
+	privatekey = rsa.PrivateKey.load_pkcs1(open('pri.pem', 'r').read())
+	# privatekey = rsa.pem.load_pem(open('pri.pem', 'r').read(), 'RSA PRIVATE KEY')
+	publickey = rsa.PublicKey.load_pkcs1(open('pub.pem', 'r').read())
+	# publickey = rsa.pem.load_pem(open('pub.pem', 'r').read().encode(encoding="utf-8"), "RSA PUBLIC KEY")
+	# publickey = privatekey
+	# with open('pub.pem') as publickfile:
+	# 	p = publickfile.read()
+	# publickey = rsa.PublicKey.load_pkcs1(p)
+	if is_encrypt:
+		if len(data) > 117:
+			tmp = ''
+			for i in range(len(data) / 117 + 1):
+				t = data[i * 117:(i + 1) * 117]
+				tmp += rsa.encrypt(t, publickey)
+			return tmp
+		else:
+			return rsa.encrypt(data, publickey)
+	else: 
+		tmp = ''
+		for i in range(len(data) / 128):
+			t = data[i * 128:(i + 1) * 128]
+			tmp += rsa.decrypt(t, privatekey)
+		return tmp
+	# rsa_Pub_Obj = RSA.load_pub_key("pub.pem")
+	# rsa_Priv_Obj = RSA.load_key("pri.pem")
+
+	# if is_encrypt:
+	# 	encrypt_msg_pri = rsa_Priv_Obj.private_encrypt(msg, RSA.pkcs1_padding)
+	# 	# encrypt_msg64_pri = encrypt_msg_pri.encode('base64').replace('\n','')
+	# 	return encrypt_msg_pri
+	# else:
+	# 	plain = rsa_Pub_Obj.public_decrypt(encrypt_msg64, RSA.pkcs1_padding)
+	# 	# plain = rsa_Pub_Obj.public_decrypt(encrypt_msg64.decode('base64'), RSA.pkcs1_padding)
+	# 	return plain
+		
+
+
+result = crypt("12345678", True)
+# print(result)
+result = crypt(result, False)
+print(result)
 
 class Client(object):
 	status = 0		#0 offline 1; 1 online
-	tickDuration = 10
+	tickDuration = 6
 	requestDuration = 10
 	captureDuration = 5;
 	tickThread = None
 	captureThread = None
 	captureTimerThread = None
+	lastCommand = None
 
 	def __init__(self):
 		super(Client, self).__init__()
@@ -43,6 +97,8 @@ class Client(object):
 			# data = urllib.urlencode(values)
 			# req = urllib2.Request(url, data, timeout=5)
 			# response = urllib2.urlopen(req)
+			# if lastCommand != command:
+				
 			print("Get command from server...")
 			self.__execShellCommand("ls -al")
 			return True
@@ -92,27 +148,26 @@ def killCapturePacketsThread(client):
 	except Exception:
 		return False
 
-client = Client()
+# client = Client()
 
-client.tickThread = threading.Thread(target=sendTickThread, args=(client,), name='tickThread')
-client.tickThread.start()
+# client.tickThread = threading.Thread(target=sendTickThread, args=(client,), name='tickThread')
+# client.tickThread.start()
 
-client.captureThread = threading.Thread(target=capturePacketsThread, args=(client,), name='captureThread')
+# client.captureThread = threading.Thread(target=capturePacketsThread, args=(client,), name='captureThread')
 
-client.captureTimerThread = threading.Thread(target=killCapturePacketsThread, args=(client,), name='killCaptureThread')
+# client.captureTimerThread = threading.Thread(target=killCapturePacketsThread, args=(client,), name='killCaptureThread')
 
 
+# i = 0
+# #Main thread
+# while True:
+# 	client.getCommandFromServer()
+# 	if i == 1:
+# 		client.captureThread.start()
+# 		client.captureTimerThread.start()
 
-i = 0
-#Main thread
-while True:
-	client.getCommandFromServer()
-	if i == 1:
-		client.captureThread.start()
-		client.captureTimerThread.start()
-
-	time.sleep(client.requestDuration)
-	i = i + 1
+# 	time.sleep(client.requestDuration)
+# 	i = i + 1
 	
 
 
